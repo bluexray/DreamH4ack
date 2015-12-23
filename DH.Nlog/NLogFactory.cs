7e13c3784cb8091d0b2eebfef9e6126391de36a3
Arthur <lichenyang@lu9.com>
2015-12-23 11:52:52 +0800

    修改个clr格式问题

--- DH.Nlog/NLogFactory.cs
+++ DH.Nlog/NLogFactory.cs
@@ -10,26 +10,14 @@ namespace DH.Nlog
 {
     public class NlogFactory:ILogFactory
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="NlogFactory"/> class.
-        /// </summary>
+
         public NlogFactory() { }
 
-        /// <summary>
-        /// Gets the logger.
-        /// </summary>
-        /// <param name="type">The type.</param>
-        /// <returns></returns>
         public ILog GetLogger(Type type)
         {
             return new NlogWrapper(type);
         }
 
-        /// <summary>
-        /// Gets the logger.
-        /// </summary>
-        /// <param name="typeName">Name of the type.</param>
-        /// <returns></returns>
         public ILog GetLogger(string typeName)
         {
             return new NlogWrapper(typeName);
--- DH.Nlog/NlogWrapper.cs
+++ DH.Nlog/NlogWrapper.cs
@@ -19,10 +19,7 @@ namespace DH.Nlog
             log = NLog.LogManager.GetLogger(typeName);
         }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="NlogWrapper"/> class.
-        /// </summary>
-        /// <param name="type">The type.</param>
+
         public NlogWrapper(Type type)
         {
             log = NLog.LogManager.GetLogger(UseFullTypeNames ? type.FullName : type.Name);
@@ -45,160 +42,105 @@ namespace DH.Nlog
             return message != null ? message.ToString() : null;
         }
 
-        /// <summary>
-        /// Logs a Debug message.
-        /// </summary>
-        /// <param name="message">The message.</param>
+
         public void Debug(object message)
         {
             if (IsDebugEnabled)
                 Log(LogLevel.Debug, AsString(message));
         }
 
-        /// <summary>
-        /// Logs a Debug message and exception.
-        /// </summary>
-        /// <param name="message">The message.</param>
-        /// <param name="exception">The exception.</param>
+
         public void Debug(object message, Exception exception)
         {
             if (IsDebugEnabled)
                 Log(LogLevel.Debug, AsString(message), exception);
         }
 
-        /// <summary>
-        /// Logs a Debug format message.
-        /// </summary>
-        /// <param name="format">The format.</param>
-        /// <param name="args">The args.</param>
+
         public void DebugFormat(string format, params object[] args)
         {
             if (IsDebugEnabled)
                 Log(LogLevel.Debug, format, args);
         }
 
-        /// <summary>
-        /// Logs a Error message.
-        /// </summary>
-        /// <param name="message">The message.</param>
+
         public void Error(object message)
         {
             if (IsErrorEnabled)
                 Log(LogLevel.Error, AsString(message));
         }
 
-        /// <summary>
-        /// Logs a Error message and exception.
-        /// </summary>
-        /// <param name="message">The message.</param>
-        /// <param name="exception">The exception.</param>
+
         public void Error(object message, Exception exception)
         {
             if (IsErrorEnabled)
                 Log(LogLevel.Error, AsString(message), exception);
         }
 
-        /// <summary>
-        /// Logs a Error format message.
-        /// </summary>
-        /// <param name="format">The format.</param>
-        /// <param name="args">The args.</param>
+
         public void ErrorFormat(string format, params object[] args)
         {
             if (IsErrorEnabled)
                 Log(LogLevel.Error, format, args);
         }
 
-        /// <summary>
-        /// Logs a Fatal message.
-        /// </summary>
-        /// <param name="message">The message.</param>
+
         public void Fatal(object message)
         {
             if (IsFatalEnabled)
                 Log(LogLevel.Fatal, AsString(message));
         }
 
-        /// <summary>
-        /// Logs a Fatal message and exception.
-        /// </summary>
-        /// <param name="message">The message.</param>
-        /// <param name="exception">The exception.</param>
+
         public void Fatal(object message, Exception exception)
         {
             if (IsFatalEnabled)
                 Log(LogLevel.Fatal, AsString(message), exception);
         }
 
-        /// <summary>
-        /// Logs a Error format message.
-        /// </summary>
-        /// <param name="format">The format.</param>
-        /// <param name="args">The args.</param>
+
         public void FatalFormat(string format, params object[] args)
         {
             if (IsFatalEnabled)
                 Log(LogLevel.Fatal, format, args);
         }
 
-        /// <summary>
-        /// Logs an Info message and exception.
-        /// </summary>
-        /// <param name="message">The message.</param>
+
         public void Info(object message)
         {
             if (IsInfoEnabled)
                 Log(LogLevel.Info, AsString(message));
         }
 
-        /// <summary>
-        /// Logs an Info message and exception.
-        /// </summary>
-        /// <param name="message">The message.</param>
-        /// <param name="exception">The exception.</param>
+
         public void Info(object message, Exception exception)
         {
             if (IsInfoEnabled)
                 Log(LogLevel.Info, AsString(message), exception);
         }
 
-        /// <summary>
-        /// Logs an Info format message.
-        /// </summary>
-        /// <param name="format">The format.</param>
-        /// <param name="args">The args.</param>
+
         public void InfoFormat(string format, params object[] args)
         {
             if (IsInfoEnabled)
                 Log(LogLevel.Info, format, args);
         }
 
-        /// <summary>
-        /// Logs a Warning message.
-        /// </summary>
-        /// <param name="message">The message.</param>
+
         public void Warn(object message)
         {
             if (IsWarnEnabled)
                 Log(LogLevel.Warn, AsString(message));
         }
 
-        /// <summary>
-        /// Logs a Warning message and exception.
-        /// </summary>
-        /// <param name="message">The message.</param>
-        /// <param name="exception">The exception.</param>
+
         public void Warn(object message, Exception exception)
         {
             if (IsWarnEnabled)
                 Log(LogLevel.Warn, AsString(message), exception);
         }
 
-        /// <summary>
-        /// Logs a Warning format message.
-        /// </summary>
-        /// <param name="format">The format.</param>
-        /// <param name="args">The args.</param>
+
         public void WarnFormat(string format, params object[] args)
         {
             if (IsWarnEnabled)
